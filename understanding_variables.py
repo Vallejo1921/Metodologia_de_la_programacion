c6464b62f69094760d38d1daff00246cbf77eb67
Maxter19 <2530453@upv.edu.mx>
2025-10-27 09:48:14 -0600

    first class

---
+++ understanding_variables.py
@@ -0,0 +1,37 @@
+message = "This is my first python variable"
+another_message = 'I am really, really, really happy'
+
+# print() -> used to show messages in terminal
+# print() -> se utiliza para mostrar mensajes en la terminal
+
+print (message)
+
+print (another_message)
+
+print (message, another_message, message)
+
+message = " I love python "
+
+print(message)
+
+"""
+Los nombres de las variables en python deben nombrarse solo con:
+
+- letras, numeros y guion bajo (espacios)
+- deben comenzar con una letra o con un guion bajo, pero NO con numero:
+    ejemplo correcto: message_1 ( :) )
+    ejemplo correcto: 1_message ( X )
+- no utiilizar espacios para  separar las palabras en los nombres de las variables
+- no utilizar pañabras reservadas de python para nombrar variables o archivos
+- deben ser cortos, pero descriptivos
+- ingles
+- minusculas
+
+"""
+Maxter_message = "Estoy aprendiendo python"
+print (Maxter_message)
+
+"""
+Traceback
+
+"""
