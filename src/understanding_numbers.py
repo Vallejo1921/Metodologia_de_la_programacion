7de51e21ee5b3e22ad3e0a5d44b28f5c6cf59328
Maxter19 <2530453@upv.edu.mx>
2025-10-31 10:19:00 -0600

    Understaning_numbers

---
+++ src/understanding_numbers.py
@@ -0,0 +1,46 @@
+# Numbers
+
+"""
+    Integers - Enteros
+
+    son los que no tienen . decimal
+    -infty , ..., -2, -1, 0, 1, 2, ..., infty
+
+    Ejemplo:
+
+       age = 33
+
+    Los podemos sumar (+)  restar (-) multiplicar (*) dividir (/)
+
+    Potencias (**2, **3)
+
+    Modulo (Dividendo%divisor)
+
+"""
+
+number_1 = 39
+number_2 = 13
+
+suma = number_1+number_2
+difference = number_1-number_2
+Multiplication = number_1*number_2
+division = number_1/number_2
+modulo = number_1%number_2
+power = number_1**2
+
+print("Suma:", suma)
+print("Difference:", difference)
+print("Multiplication:", Multiplication)
+print("Division:", division)
+print("Modulo:", modulo)
+print("Power:", power)
+
+
+print("La suma del tipo", type(suma))
+print("La difference es del tipo", type(difference))
+print("La multiplication es de tipo", type(Multiplication))
+print("La division es de el tipo", type(division))
+print("La modulo es del tipo", type(modulo))
+print("La power es del tipo", type(power))
+
+#
